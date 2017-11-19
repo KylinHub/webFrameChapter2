e0d4242e1faa2a117c3b33a5107dd4a3e0eeb1c1
MerlinIdea <2801646618@qq.com>
2017-11-19 18:05:13 +0800

    2017-11-19 18:04

--- src/main/java/org/smart4j/chapter2/controller/CustomerCreateServlet.java
+++ src/main/java/org/smart4j/chapter2/controller/CustomerCreateServlet.java
@@ -1,9 +1,9 @@
 package org.smart4j.chapter2.controller;
 
+import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by 28016 on 2017/11/19.
@@ -11,11 +11,14 @@ import java.io.IOException;
 
 @WebServlet("/customer_create")
 public class CustomerCreateServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws javax.servlet.ServletException, IOException {
 
-    }
+  protected void doPost(HttpServletRequest req, HttpServletResponse res)
+      throws javax.servlet.ServletException, IOException {
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+  }
 
-    }
+  protected void doGet(HttpServletRequest req, HttpServletResponse res)
+      throws javax.servlet.ServletException, IOException {
+
+  }
 }
--- src/main/java/org/smart4j/chapter2/model/Customer.java
+++ src/main/java/org/smart4j/chapter2/model/Customer.java
@@ -6,58 +6,58 @@ package org.smart4j.chapter2.model;
  */
 public class Customer {
 
-    // ID
-    private long id;
+  // ID
+  private long id;
 
-    // 客户名称
-    private String name;
+  // 客户名称
+  private String name;
 
-    // 联系人
-    private String contact;
+  // 联系人
+  private String contact;
 
-    // 电话号码
-    private String telephone;
+  // 电话号码
+  private String telephone;
 
-    // 备注
-    private String remark;
+  // 备注
+  private String remark;
 
-    public long getId() {
-        return id;
-    }
+  public long getId() {
+    return id;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getContact() {
-        return contact;
-    }
+  public String getContact() {
+    return contact;
+  }
 
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
+  public void setContact(String contact) {
+    this.contact = contact;
+  }
 
-    public String getTelephone() {
-        return telephone;
-    }
+  public String getTelephone() {
+    return telephone;
+  }
 
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
+  public void setTelephone(String telephone) {
+    this.telephone = telephone;
+  }
 
-    public String getRemark() {
-        return remark;
-    }
+  public String getRemark() {
+    return remark;
+  }
 
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
+  public void setRemark(String remark) {
+    this.remark = remark;
+  }
 }
