0fc3e6aa279f3a612a7cd42ea035aefa4645b517
MerlinIdea <2801646618@qq.com>
2017-11-19 17:50:42 +0800

    11-19

---
+++ src/main/java/org/smart4j/chapter2/controller/CustomerCreateServlet.java
@@ -0,0 +1,21 @@
+package org.smart4j.chapter2.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 28016 on 2017/11/19.
+ */
+
+@WebServlet("/customer_create")
+public class CustomerCreateServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/org/smart4j/chapter2/model/Customer.java
@@ -0,0 +1,63 @@
+package org.smart4j.chapter2.model;
+
+/**
+ * 客户
+ * Created by 28016 on 2017/11/19.
+ */
+public class Customer {
+
+    // ID
+    private long id;
+
+    // 客户名称
+    private String name;
+
+    // 联系人
+    private String contact;
+
+    // 电话号码
+    private String telephone;
+
+    // 备注
+    private String remark;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
